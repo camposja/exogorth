18a68fe2a94c52fc08015e10938b44c14200877b
Jose A. Campos <jjoseccampos@gmail.com>
2017-09-16 15:00:55 -0400

    building requirement 2 opening crawl

--- alderaan.rb
+++ alderaan.rb
@@ -16,10 +16,28 @@ response = HTTParty.get(film_url)
 json = JSON.parse(response.body)
 films = json["results"]
 
-films.each_with_index do |film, index|
-  puts "#{index + 1} - #{film["title"]}"
-end
+loop do
+  puts "0 - exit"
+  films.each_with_index do |film, index|
+    puts "#{index + 1} - #{film["title"]}"
+  end
 
-print "What film do you want informaion on? "
-choice = gets.chomp.to_i
-puts "You chose #{choice}"
+  print "What film do you want informaion on? "
+  choice = gets.chomp.to_i
+  if choice == 0
+    exit
+  end
+
+  # When a user selects a title, they are presented with the opening crawl for the movie
+  # and asked if they would like to learn more or select another title.
+
+  film = films[choice - 1]
+  puts film["opening_crawl"]
+
+  print "Learn more? [y/n]: "
+  choice = gets.chomp.downcase
+  if choice == "y"
+    # show more
+    puts "Show more"
+  end
+end
